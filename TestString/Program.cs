bae7089562e8e6ac12c6a86788d4da5233dd2e73
maciej.nowicki <virtual92@gmail.com>
2015-01-04 21:55:07 +0100

    Add Project

---
+++ TestString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleJSON;
+
+namespace TestString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var foo = new JSONClass();
+            foo["key"] = "foo";
+            String a = "a" + foo;
+            String b = "b" + foo.ToString();
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.Read();
+        }
+    }
+}
